18f89b3cae5099b88a7a5603f06cd443d26ef627
Felix Triebel <felix@triebel.pro>
2020-06-11 07:41:07 +0200

    Set fixed daytimes for DARK_MODE

--- shell/day_night_mode.sh
+++ shell/day_night_mode.sh
@@ -1,8 +1,8 @@
 # adjust color scheme according to sunrise/sunset
-sunrise=$(sunwait -p 49N 9E | grep -i rise | awk '{ print $3 }')
-sunset=$(sunwait -p 49N 9E | grep -i rise | awk '{ print $6 }')
+# sunrise=$(sunwait -p 49N 9E | grep -i rise | awk '{ print $3 }')
+# sunset=$(sunwait -p 49N 9E | grep -i rise | awk '{ print $6 }')
 date=$(date +"%H%M")
-if [ "$sunrise" -lt "$date" ] && [ "$date" -lt "$sunset" ] ; then
+if [ "0700" -lt "$date" ] && [ "$date" -lt "1900" ] ; then
     export DARK_MODE=0
 else
     export DARK_MODE=1
