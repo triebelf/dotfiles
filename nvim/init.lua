8b540785f5c0d1265dcb89bfb81f572333c1307e
Triebel Felix (XC-AD/ETV5) <felix.triebel@de.bosch.com>
2022-02-06 20:28:56 +0100

    fix: nvim setting - netrw_list_hide

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,7 +28,7 @@ vim.g.netrw_browse_split = 4
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = 15
 vim.g.netrw_banner = 0
-vim.g.netrw_list_hide = vim.opt.wildignore:get()
+vim.g.netrw_list_hide = table.concat(vim.opt.wildignore:get(), ",")
 vim.g.netrw_hide = 1
 vim.g.netrw_keepdir = 0
 
