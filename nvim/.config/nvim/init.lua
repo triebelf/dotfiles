2aff630cd5a6bfc2c9c1dec22cca324b16515f2f
Felix Triebel <felix.triebel@de.bosch.com>
2024-05-16 15:17:46 +0200

    Fix lua language server configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -231,14 +231,16 @@ require("lspconfig").lua_ls.setup({
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
+                path = vim.split(package.path, ";"),
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
                 globals = { "vim" },
             },
             workspace = {
-                checkThirdParty = true,
+                -- Make the server aware of Neovim runtime files and plugins
                 library = { vim.env.VIMRUNTIME },
+                checkThirdParty = false,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
