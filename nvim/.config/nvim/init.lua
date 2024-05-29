5796bae81a78e3ed4c0a5496ea90f6cb0591e169
Felix Triebel <felix.triebel@de.bosch.com>
2024-05-29 16:44:02 +0200

    clangd unlimited resources

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -200,7 +200,7 @@ lsp_defaults.capabilities =
     vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 require("lspconfig").bashls.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").clangd.setup({
-    cmd = { "clangd", "--offset-encoding=utf-16" },
+    cmd = { "clangd", "--offset-encoding=utf-16", "--limit-references=0" },
     flags = { debounce_text_changes = 150 },
 })
 require("clangd_extensions").setup({})
