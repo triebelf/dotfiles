d56f1e8e0165c0519b579ba408a5c39666108493
Felix Triebel <felix@triebel.pro>
2025-02-03 20:01:10 +0100

    Update submodules

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -84,7 +84,7 @@ vim.opt.number = true
 vim.opt.signcolumn = "yes"
 
 -- theme
-vim.cmd([[colorscheme modus]]) -- modus_operandi, modus_vivendi
+vim.cmd([[colorscheme modus]])
 --vim.o.background = "light"
 
 -- lualine.nvim
@@ -92,10 +92,10 @@ require("lualine").setup({ options = { icons_enabled = false } })
 
 -- nvim-treesitter
 require("nvim-treesitter.configs").setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 })
 
 -- folding
@@ -108,15 +108,15 @@ vim.opt.foldlevelstart = 3
 -- plenary.nvim
 -- telescope.nvim
 require("telescope").setup({
-    defaults = { layout_strategy = "vertical", layout_config = { width = 0.95 } },
-    pickers = {
-        lsp_dynamic_workspace_symbols = { fname_width = 60 },
-        live_grep = {
-            additional_args = function(opts)
-                return { "--hidden", "--iglob", "!.git" }
-            end,
-        },
-    },
+	defaults = { layout_strategy = "vertical", layout_config = { width = 0.95 } },
+	pickers = {
+		lsp_dynamic_workspace_symbols = { fname_width = 60 },
+		live_grep = {
+			additional_args = function(opts)
+				return { "--hidden", "--iglob", "!.git" }
+			end,
+		},
+	},
 })
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
@@ -125,76 +125,76 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local cmp = require("cmp")
 local select_opts = { behavior = cmp.SelectBehavior.Select }
 cmp.setup({
-    completion = { autocomplete = false },
-    sources = cmp.config.sources({
-        -- cmp-nvim-lsp
-        { name = "nvim_lsp" },
-        -- cmp-snippy
-        { name = "snippy" },
-        -- cmp-treesitter
-        { name = "treesitter" },
-        -- cmp-nvim-lua
-        { name = "nvim_lua" },
-        -- cmp-buffer
-        { name = "buffer" },
-    }),
-    mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            local col = vim.fn.col(".") - 1
-            if cmp.visible() then
-                cmp.select_next_item(select_opts)
-            elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-                fallback()
-            else
-                cmp.complete()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item(select_opts)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
-    snippet = {
-        expand = function(args)
-            -- nvim-snippy
-            -- vim-snippets
-            require("snippy").expand_snippet(args.body)
-        end,
-    },
-    sorting = {
-        comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            require("clangd_extensions.cmp_scores"),
-            cmp.config.compare.locality,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-        },
-    },
+	completion = { autocomplete = false },
+	sources = cmp.config.sources({
+		-- cmp-nvim-lsp
+		{ name = "nvim_lsp" },
+		-- cmp-snippy
+		{ name = "snippy" },
+		-- cmp-treesitter
+		{ name = "treesitter" },
+		-- cmp-nvim-lua
+		{ name = "nvim_lua" },
+		-- cmp-buffer
+		{ name = "buffer" },
+	}),
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			local col = vim.fn.col(".") - 1
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_opts)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	snippet = {
+		expand = function(args)
+			-- nvim-snippy
+			-- vim-snippets
+			require("snippy").expand_snippet(args.body)
+		end,
+	},
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			cmp.config.compare.recently_used,
+			require("clangd_extensions.cmp_scores"),
+			cmp.config.compare.locality,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 })
 
 cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = { { name = "buffer" } },
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = { { name = "buffer" } },
 })
 
 -- cmp-path
 -- cmp-cmdline
 cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 })
 
 -- mason
@@ -205,84 +205,84 @@ require("mason-lspconfig").setup({ automatic_installation = true })
 vim.lsp.set_log_level("off")
 local lsp_defaults = require("lspconfig").util.default_config
 lsp_defaults.capabilities =
-    vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+	vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 require("lspconfig").bashls.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").clangd.setup({
-    cmd = {
-        "clangd",
-        "--offset-encoding=utf-16",
-        "--limit-references=0",
-        "--limit-results=0",
-        "--rename-file-limit=0",
-    },
-    flags = { debounce_text_changes = 150 },
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+		"--limit-references=0",
+		"--limit-results=0",
+		"--rename-file-limit=0",
+	},
+	flags = { debounce_text_changes = 150 },
 })
 require("clangd_extensions").setup({})
 require("lspconfig").cmake.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").dockerls.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").esbonio.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").typos_lsp.setup({
-    root_dir = require("lspconfig").util.root_pattern("pyproject.toml"),
-    flags = { debounce_text_changes = 150 },
+	root_dir = require("lspconfig").util.root_pattern("pyproject.toml"),
+	flags = { debounce_text_changes = 150 },
 })
 require("lspconfig").jsonls.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").lemminx.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").taplo.setup({ flags = { debounce_text_changes = 150 } })
 require("lspconfig").ltex.setup({
-    flags = { debounce_text_changes = 150 },
-    settings = {
-        ltex = {
-            enabled = false,
-            language = "en-US",
-            additionalRules = { motherTongue = "de-DE" },
-            dictionary = {
-                ["en-US"] = { "deserializability", "deserializable", "deserialization", "serializability" },
-            },
-        },
-    },
+	flags = { debounce_text_changes = 150 },
+	settings = {
+		ltex = {
+			enabled = false,
+			language = "en-US",
+			additionalRules = { motherTongue = "de-DE" },
+			dictionary = {
+				["en-US"] = { "deserializability", "deserializable", "deserialization", "serializability" },
+			},
+		},
+	},
 })
 require("lspconfig").pyright.setup({
-    settings = {
-        python = { analysis = { autoSearchPaths = true, diagnosticMode = "workspace", useLibraryCodeForTypes = true } },
-    },
-    flags = { debounce_text_changes = 150 },
+	settings = {
+		python = { analysis = { autoSearchPaths = true, diagnosticMode = "workspace", useLibraryCodeForTypes = true } },
+	},
+	flags = { debounce_text_changes = 150 },
 })
 require("lspconfig").lua_ls.setup({
-    settings = {
-        Lua = {
-            runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
-            diagnostics = { globals = { "vim" } },
-            workspace = { library = { vim.env.VIMRUNTIME }, checkThirdParty = true, preloadFileSize = 10000 },
-            telemetry = { enable = false },
-        },
-    },
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+			diagnostics = { globals = { "vim" } },
+			workspace = { library = { vim.env.VIMRUNTIME }, checkThirdParty = true, preloadFileSize = 10000 },
+			telemetry = { enable = false },
+		},
+	},
 })
 require("lspconfig").yamlls.setup({ flags = { debounce_text_changes = 150 } })
 
 -- nvim-lint
 require("lint").linters_by_ft = {
-    cpp = { "cspell" },
-    python = { "mypy", "pylint" },
-    yaml = { "yamllint" },
-    sh = { "bash" },
-    make = { "checkmake" },
-    cmake = { "cmakelint" },
+	cpp = { "cspell" },
+	python = { "mypy", "pylint" },
+	yaml = { "yamllint" },
+	sh = { "bash" },
+	make = { "checkmake" },
+	cmake = { "cmakelint" },
 }
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-    callback = function()
-        require("lint").try_lint()
-    end,
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
 
 -- conform.nvim
 require("conform").setup({
-    formatters_by_ft = {
-        cpp = { "clang_format" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        sh = { "shellharden" },
-        yaml = { "yq" },
-    },
+	formatters_by_ft = {
+		cpp = { "clang_format" },
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		sh = { "shellharden" },
+		yaml = { "yq" },
+	},
 })
 
 -- outline
